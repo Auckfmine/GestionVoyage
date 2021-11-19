936847c7dfc4e1f4643c825623fd6abebbfbc1d4
MortadhaGaied <mortadha.gaied@esprit.tn>
2021-11-19 01:25:16 +0100

    tache user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+
+
+        return $this->render('user/login.html.twig');
+    }
+
+    /**
+     * @Route("/user", name="user")
+     */
+    public function user()
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('user/index.html.twig', [
+            "users" => $users,
+        ]);
+    }
+    /**
+     * @Route("/add-user", name="add_user")
+     */
+    public function addUser(Request $request): Response
+    {
+        $datetime = new DateTime();
+        $user = new User();
+        $user->setCreatedDateUser(new \DateTime());
+        $user->setLastUpdatedUser(new \DateTime());
+        $form=$this->createForm(UserFormType::class,$user);
+        $form->handleRequest($request);
+        if( $form->isSubmitted() && $form->isValid()){
+            $entityManager=$this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render("user/_form.html.twig", [
+            "form" => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/modify-user/{id}", name="modify_user")
+     */
+    public function modifyUser(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->flush();
+            return $this->redirectToRoute('user', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render("user/_form.html.twig", [
+            "form_title" => "Modify user",
+            "form" => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/delete-user/{id}", name="delete_user")
+     */
+    public function deleteUser(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("user");
+    }
+}
---
+++ src/Form/UserFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('first_name')
+            ->add('last_name')
+            ->add('email')
+            ->add('number')
+            ->add('username')
+            ->add('password',PasswordType::class)
+            ->add('role',ChoiceType::class, [
+                'choices' => [
+                    'CLIENT'=>'CLIENT',
+                    'ADMIN'=>'ADMIN',
+                    'RESPONSABLE_ABONNEMENT'=>'RESPONSABLE_ABONNEMENT',
+                    'RESPONSABLE_RECLAMATION'=>'RESPONSABLE_RECLAMATION',
+                    'RESPONSABLE_MDT'=>'RESPONSABLE_MDT',
+                    'RESPONSABLE_VOYAGE'=>'RESPONSABLE_VOYAGE',
+                    'RESPONSABLE_RESERVATION'=>'RESPONSABLE_RESERVATION',
+                ],])
+            ->add('birthday', DateType::class, [
+                'widget' => 'single_text'])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
