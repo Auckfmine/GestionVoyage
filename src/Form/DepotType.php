415c484ad4cd4cf7542ec38095316d5f71317b8a
MoyenTransport <mahdi.homrani@esprit.tn>
2021-12-09 11:10:27 +0100

    09/12

--- src/Form/DepotType.php
+++ src/Form/DepotType.php
@@ -27,12 +27,33 @@ class DepotType extends AbstractType
                     'Metro'=>'Metro',
                     'Train '=>'Train'
                 ],])
-            ->add('Localisation',TextType::class,[
-                'attr'=>[
-                    'placeholder'=>'Spécifier la localisation du Depot',
-                    'class'=> 'custom_class'
-                ]
-            ])
+            ->add('Localisation',ChoiceType::class, [
+                'choices' => [
+                    'Tunis'=>'Tunis',
+                    'Ben Arous'=>'Ben Arous',
+                    'Manouba '=>'Manouba',
+                    'Ariana '=>'Ariana',
+                    'Bizerte '=>'Bizerte',
+                    'Béja '=>'Béja',
+                    'Gabès '=>'Gabès',
+                    'Gafsa '=>'Gafsa',
+                    'Jendouba '=>'Jendouba',
+                    'Kairouan '=>'Kairouan',
+                    'Kasserine'=>'Kasserine',
+                    'Kébili'=>'Kébili',
+                    'Le Kef '=>'Le Kef',
+                    'Mahdia '=>'Mahdia',
+                    'Médenine '=>'Médenine',
+                    'Monastir '=>'Monastir',
+                    'Nabeul '=>'Nabeul',
+                    'Sfax '=>'Sfax',
+                    'Sidi Bouzid '=>'Sidi Bouzid',
+                    'Siliana '=>'Siliana',
+                    'Sousse '=>'Sousse',
+                    'Tataouine '=>'Tataouine',
+                    'Tozeur '=>'Tozeur',
+                    'Zaghouan '=>'Zaghouan',
+                ],])
             ->add('Etat',ChoiceType::class, [
                 'choices' => [
                     'En_marche'=>'En_marche',
